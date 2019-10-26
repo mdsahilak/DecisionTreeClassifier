743dd5d78f23da9962c03f6a4099400702f6b3b8
Muhammed Sahil AK <49957745+mdsahilak@users.noreply.github.com>
2019-10-26 20:05:33 +0530

    Update Dataset.swift

--- Swift/JustTheCode/Dataset.swift
+++ Swift/JustTheCode/Dataset.swift
@@ -1,1 +1,4 @@
 
+# Just the code used in the playground.
+## This directory is intended for people who would not like to download the playground file or do not use xcode playgrounds but would like to look at the code.
+### This is not intended to run. It is just for inspecting code right on github. As of now github does not allow you to view a .playground file's source. The main run script is in the main.swift file and the others include the dataset, helper funcs, etc.
